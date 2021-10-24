0e6d6f2fc8eef842a62dfde2fb62d45128d63b7a
knaresh545 <91107057+knaresh545@users.noreply.github.com>
2021-10-24 18:44:06 +0530

    [Naresh] ADDED: Added UserCase 1

--- Program.cs
+++ Program.cs
@@ -7,6 +7,10 @@ namespace EmployeeWageCalculation
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation");
+
+            UC_1 uc1 = new UC_1();
+            uc1.Attendance();
         }
     }
+    
 }
---
+++ UC_1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EmployeeWageCalculation
+{
+    class UC_1
+    {
+        public const int IS_FULL_TIME = 1;
+        public void Attendance()
+        {
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee FULL TIME Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
