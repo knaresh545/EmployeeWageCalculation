dad6bfd4d9ba6924c34cec53867f4a3146116962
knaresh545 <91107057+knaresh545@users.noreply.github.com>
2021-10-24 19:33:25 +0530

    [Naresh] ADDED: Added UserCase 5

--- Program.cs
+++ Program.cs
@@ -7,6 +7,9 @@ namespace EmployeeWageCalculation
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation");
+
+            UC_5 uc5 = new UC_5();
+            uc5.MonthlyWage();
         }
     }
 }
---
+++ UC_5.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace EmployeeWageCalculation
+{
+    class UC_5
+    {
+        public const int IS_FULL_TIME = 1;
+        public const int IS_PART_TIME = 2;
+
+        public void MonthlyWage()
+        {
+            const int EMP_RATE_PER_HOUR = 20;
+            const int NUM_WORKING_DAYS = 20;
+            int empHrs = 0;
+            int empWage = 0;
+            int totalEmpWage = 0;
+
+
+            for (int day = 1; day <= NUM_WORKING_DAYS; day++)
+            {
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                empWage = empHrs * EMP_RATE_PER_HOUR;
+                totalEmpWage += empWage;
+                Console.WriteLine("Employee Day# " + day + " Wage is :" + empWage);
+            }
+            Console.WriteLine("Monthly Employee Wage :" + totalEmpWage);
+        }
+    }
+}
