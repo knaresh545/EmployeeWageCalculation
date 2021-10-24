1230166ea828a2e868ccc49f044508e3dbb8b9a4
knaresh545 <91107057+knaresh545@users.noreply.github.com>
2021-10-24 19:26:03 +0530

    [Naresh] ADDED: Added UserCase 4

---
+++ UC_4.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace EmployeeWageCalculation
+{
+    class UC_4
+    {
+        public const int IS_FULL_TIME = 1;
+        public const int IS_PART_TIME = 2;
+
+        public void Case()
+        {
+
+            const int EMP_RATE_PER_HOUR = 20;
+            int empHrs = 0;
+            int empWage = 0;
+            Random random = new Random();
+            int empCheck = random.Next(0, 3);
+
+            switch (empCheck)
+            {
+                case IS_FULL_TIME:
+                    Console.WriteLine("Employee Full TIME Present");
+                    empHrs = 8;
+                    break;
+                case IS_PART_TIME:
+                    Console.WriteLine("Employee PART TIME Present");
+                    empHrs = 4;
+                    break;
+                default:
+                    Console.WriteLine("Employee is Absent");
+                    empHrs = 0;
+                    break;
+            }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Employee Daily Wage :" + empWage);
+        }
+    }
+}
