3c229022674d249c14d6afe7c147c45b131256e1
knaresh545 <91107057+knaresh545@users.noreply.github.com>
2021-10-24 22:40:26 +0530

    [Naresh] ADDED: added UserCase 8

--- Program.cs
+++ Program.cs
@@ -7,6 +7,10 @@ namespace EmployeeWageCalculation
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation");
+
+            UC_8 company = new UC_8();
+            company.ComputeEmpWage("Dmart", 20, 20, 100);
+            company.ComputeEmpWage("Reliance", 15, 25, 150);
         }
     }
 }
